7e5c594522c5058f99d3b72f60d9a0cecd3d7b44
tomiock <ockier1@gmail.com>
2024-04-21 16:20:30 +0200

    formatting

--- main.go
+++ main.go
@@ -1,20 +1,21 @@
-package main 
+package main
+
 // always main package for executable program
 
 import (
-    "fmt"
-    "os"
+	"fmt"
+	"os"
 
-    "mdcreator/html" // to access the functions use html.<function_name>
+	"mdcreator/html" // to access the functions use html.<function_name>
 )
 
 
 func main() {
 
-    if len(os.Args) < 2 {
-	fmt.Println("Usage: main <input.md>")
-	os.Exit(1)
-    }
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: main <input.md>")
+		os.Exit(1)
+	}
 
-    html.WriteHTMLFile(os.Args[1])
+	html.WriteHTMLFile(os.Args[1])
 }
